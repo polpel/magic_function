81976c943c8de9c31802c20361a0dac55ea194b7
Paolo <paolo.pelucchi@yahoo.com>
2021-07-15 15:43:55 +0200

    Comment the program and add Contributing section to README

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # UVtutrepo
 
-Small and simple repo for the tutorial.  
+Small and simple repo for the git tutorial.  
 
 ## Description and usage
+ 
+Give three numbers to the magic function and who knows what you might get out!
 
-Implements a small piece of code. 
-Give three numbers to the function and who knows what you might get out!
+## Contributing
+
+Feel free to add your own fun tiny functions to the magic function! 
+The result should be something printed or displayed.
--- program.py
+++ program.py
@@ -3,20 +3,39 @@ import matplotlib.pyplot as plt
 import random
 
 def magic_function(a, b, c):
+    """Returns something random made from three input numbers
+    
+    A collection of tiny functions that take three numbers and print or
+    display something made from them. The function that is actually 
+    used at each call of magic_function() is chosen at random.
+
+    Args:
+      a, b, c:
+        Three numbers, any type is fine.
+
+    Returns:
+      A surprise :)
+    """
+    
+    # List of tiny functions implemented
     options = ['add', 'mult', 'rgb']
 
     do = random.choice(options)
-
+    
+    # Adds the numbers together
     if do == 'add':
         sum = a + b + c
         print(f'Adding {a}, {b} and {c} gives {sum}!')
         return
-
+    
+    # Multiplies the numbers together
     if do == 'mult':
         product = a * b * c
         print(f'{a} times {b} times {c} equals {product}!')
         return
 
+    # Scales the numbers to 0--1 range (treating largest as 1) and  
+    # treats them as RGB values to display a colour
     if do == 'rgb':
         largest = max(a, b, c)
         a_rgb = a / largest
